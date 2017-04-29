f20b210aeca6b6ee06fc3c3e54769a7606900057
Douman <douman@gmx.se>
2017-04-29 12:40:17 +0300

    0.2.2

--- ci/upload_arts.sh
+++ ci/upload_arts.sh
@@ -8,6 +8,8 @@ sh "$(dirname "$0")/coverage.sh"
 
 ## Configure Travis
 COMMIT_AUTHOR_EMAIL=`git --no-pager log -1 --pretty=format:"%ae" HEAD`
+REPO=`git config remote.origin.url`
+SHA=`git log -1 --format="%s(%h %cd)" --date=short`
 
 git config --global user.name "Travis CI"
 git config --global user.email "$COMMIT_AUTHOR_EMAIL"
@@ -15,14 +17,12 @@ echo "https://${GIT_TOKEN}:x-oauth-basic@github.com\n" > ~/.git-credentials
 
 ## Upload HTML docs
 rm -rf html/
-mkdir html/
+
+git clone $REPO html/
+
 cd html/
 
-git clone $REPO .
-
 git checkout gh-pages || git checkout --orphan gh-pages
-
-ls
 
 find . -name "_*.*" | xargs rm -rf
 find . -name ".*.*" | xargs rm -rf
@@ -33,7 +33,7 @@ cd ../
 
 npm run docs
 
-cd html
+cd html/
 
 git status
 
